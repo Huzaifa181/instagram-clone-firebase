37fbccc8476e229be2e7ffd6746aab48574334ae
Huzaifa181 <huzaifa.ahmed181@gmail.com>
2021-06-11 04:11:17 +0500

    Made the front end structure which includes post section as well

---
+++ src/Post.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Post.css'
+import Avatar from "@material-ui/core/Avatar"
+const Post = ({username, caption, imageUrl}) => {
+    return (
+        <div className="post">
+            <div className="post__header">
+            <Avatar
+            className='post__avatar'
+            alt="Huzaifa"
+            />
+            <h3>
+                {username}
+            </h3>
+            </div>
+            <img
+                width={120}
+                className='post__image'
+                src={imageUrl}/>
+            <h4 className='post__text'>
+                <strong>{username}: </strong> {caption}
+            </h4>
+        </div>
+    )
+}
+
+export default Post
